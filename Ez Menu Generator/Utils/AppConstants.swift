3991a2be3521337e3afc89fd8ac0e02d2ce581f4
r0gva1v <83495810+rogvaiv-bla@users.noreply.github.com>
2026-03-15 21:48:19 +0100

    Task 1: Complete file structure reorganization - move ViewModels, Services, Utils, and Theme to proper folders

---
+++ Ez Menu Generator/Utils/AppConstants.swift
@@ -0,0 +1,8 @@
+import Foundation
+
+enum AppConstants {
+    static let appName = "Ez Menu Generator"
+    static let version = "1.0.0"
+    static let maxMenus = 20
+    static let maxRecipes = 100
+}
